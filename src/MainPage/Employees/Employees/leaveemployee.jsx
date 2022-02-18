ff2388b932899940fd887c09cfff6b7b8f6ca3e2
yasith wimukthi <it19966922@my.sliit.lk>
2022-02-18 13:23:51 +0530

    implement leave settings page

--- src/MainPage/Employees/Employees/leaveemployee.jsx
+++ src/MainPage/Employees/Employees/leaveemployee.jsx
@@ -110,7 +110,6 @@ const LeaveEmployee = () => {
                 </div>
               </div>
             ),
-      
         
         },
         
--- src/MainPage/Employees/Employees/leavesettings.jsx
+++ src/MainPage/Employees/Employees/leavesettings.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Helmet} from "react-helmet";
 import {Link} from 'react-router-dom';
 import {Avatar_02} from "../../../Entryfile/imagepath"
 import moment from "moment";
+import axios from "axios";
 
 const LeaveSettings = () => {
+
+    const [leaveList, setLeaveList] = useState([]);
+
+    useEffect(() => {
+        getLeaves()
+    }, []);
+
+    const getLeaves = async () => {
+        const res = await axios.get(' http://127.0.0.1:8000/api/leaves/');
+        setLeaveList(res.data);
+    }
+
     return (
         <div className="page-wrapper">
             <Helmet>
@@ -44,10 +57,66 @@ const LeaveSettings = () => {
                                 </thead>
                                 <tbody>
                                 {
-
-                                }
+                                    leaveList && leaveList.length > 0 && leaveList.map((leave,index)=>(
+                                        <tr key={index}>
+                                            <td>{index+1}</td>
+                                            <td>{leave.country}</td>
+                                            <td>{leave.leave_name}</td>
+                                            <td></td>
+                                            <td></td>
+                                            <td className="text-right">
+                                                <div className='dropdown dropdown-action'>
+                                                    <a href="#" className="action-icon dropdown-toggle"
+                                                       data-toggle="dropdown"
+                                                       aria-expanded="false"><i className="material-icons">more_vert</i></a>
+                                                    <div className="dropdown-menu dropdown-menu-right">
+                                                        <a className="dropdown-item" href="#" data-toggle="modal"
+                                                           data-target="#edit_leave_type"
+                                                           ><i
+                                                            className="fa fa-pencil m-r-5"/> Edit</a>
+                                                        <a className="dropdown-item" href="#" data-toggle="modal"
+                                                           data-target="#delete_company"
+                                                           ><i
+                                                            className="fa fa-trash-o m-r-5"/> Delete</a>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                             </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Edit Holiday Modal */}
+            <div className="modal custom-modal fade" id="edit_leave_type" role="dialog">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Edit Holiday</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="form-group">
+                                    <label>Holiday Name <span className="text-danger">*</span></label>
+                                    <input className="form-control" defaultValue="New Year" type="text"/>
+                                </div>
+                                <div className="form-group">
+                                    <label>Holiday Date <span className="text-danger">*</span></label>
+                                    <div><input className="form-control"
+                                                type="date" />
+                                    </div>
+                                </div>
+                                <div className="submit-section">
+                                    <button type="submit" data-dismiss="modal" className="btn btn-primary submit-btn">Save
+                                    </button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
